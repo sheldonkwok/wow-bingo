b0768ad0edd4cd871222c88ad2f47dceddaf3906
Sheldon Kwok <sheldon@secondspectrum.com>
2018-08-30 19:48:40 -0700

    Fill the board

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,36 @@
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
 import * as http from 'http';
 import * as cp from 'child_process';
 import * as micro from 'micro';
+import * as lodash from 'lodash';
+
+const { captions } = yaml.safeLoad(fs.readFileSync('./config.yml', 'utf8'));
+
+const TOTAL_SQUARES = 25;
+const FREE_SQUARE = 12;
+const CAPTION_IMGS = Array.from({ length: captions.length - 1 }, (_, i) => ++i).map(i => `dist/${i}.png`);
+const BASE_MONTAGE_ARGS = ['-font', 'Arial', '-tile', '5x5', '-border', '1', '-bordercolor', 'black', '-'];
+
+function createBoardProcess(): cp.ChildProcess {
+  const imgs = lodash.shuffle(CAPTION_IMGS).slice(0, TOTAL_SQUARES);
+  imgs[FREE_SQUARE] = 'dist/0.png';
+
+  const args = imgs.concat(BASE_MONTAGE_ARGS);
+  return cp.spawn('montage', args);
+}
 
 async function main(_req: http.IncomingMessage, res: http.ServerResponse) {
   res.setHeader('Content-Type', 'image/png');
 
-  const args = [
-    'dist/1.png',
-    'dist/2.png',
-    'dist/3.png',
-    'dist/4.png',
-    '-font',
-    'Arial',
-    '-tile',
-    '2x2',
-    '-border',
-    '1',
-    '-bordercolor',
-    'black',
-    '-',
-  ];
+  const child = createBoardProcess();
 
-  const child = cp.spawn('montage', args);
-
+  child.once('error', () => micro.send(res, 500, 'Error creating bingo card'));
   child.once('exit', code => {
-    if (code !== 0) {
-      micro.send(res, 500, 'Error creating bingo card');
-    }
-  });
-
-  child.once('error', () => {
-    micro.send(res, 500, 'Error creating bingo card');
+    if (code !== 0) micro.send(res, 500, 'Error creating bingo card');
   });
 
   child.stderr.on('data', err => console.log(err.toString()));
-
   micro.send(res, 200, child.stdout);
 }
 
